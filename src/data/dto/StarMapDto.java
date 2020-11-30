2bfdf9ca009f6c10fd44fcf133c3085de156564a
bitcamp <bitcamp@DESKTOP-E9JCRHH>
2020-11-30 15:49:32 +0900

    dao dto 보냅니다

---
+++ src/data/dto/StarMapDto.java
@@ -0,0 +1,72 @@
+package data.dto;
+
+
+
+public class StarMapDto {
+	
+	private String shopnum;
+	private String shopname;
+	private String shophp;
+	private String shopaddr;
+	private String shopaddrdetail;
+	private String shopphoto;
+	private String shopdetail;
+	private String mpositionx;
+	private String mpositiony;
+	
+	public String getShopnum() {
+		return shopnum;
+	}
+	public void setShopnum(String shopnum) {
+		this.shopnum = shopnum;
+	}
+	public String getShopname() {
+		return shopname;
+	}
+	public void setShopname(String shopname) {
+		this.shopname = shopname;
+	}
+	public String getShophp() {
+		return shophp;
+	}
+	public void setShophp(String shophp) {
+		this.shophp = shophp;
+	}
+	public String getShopaddr() {
+		return shopaddr;
+	}
+	public void setShopaddr(String shopaddr) {
+		this.shopaddr = shopaddr;
+	}
+	public String getShopaddrdetail() {
+		return shopaddrdetail;
+	}
+	public void setShopaddrdetail(String shopaddrdetail) {
+		this.shopaddrdetail = shopaddrdetail;
+	}
+	public String getShopphoto() {
+		return shopphoto;
+	}
+	public void setShopphoto(String shopphoto) {
+		this.shopphoto = shopphoto;
+	}
+	public String getShopdetail() {
+		return shopdetail;
+	}
+	public void setShopdetail(String shopdetail) {
+		this.shopdetail = shopdetail;
+	}
+	public String getMpositionx() {
+		return mpositionx;
+	}
+	public void setMpositionx(String mpositionx) {
+		this.mpositionx = mpositionx;
+	}
+	public String getMpositiony() {
+		return mpositiony;
+	}
+	public void setMpositiony(String mpositiony) {
+		this.mpositiony = mpositiony;
+	}
+	
+}
