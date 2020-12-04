65b57f226f31b8e5eae38b07f649e22b80944dee
jjeong <jjeong@localhost>
2020-12-04 15:56:51 +0900

    orderchecklist 추가

--- src/data/dao/FOrderDao.java
+++ src/data/dao/FOrderDao.java
@@ -8,53 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import data.dto.FOrderDto;
-import data.dto.MenuDto;
 import mysql.db.MysqlConnect;
 
 public class FOrderDao {
 	MysqlConnect db = new MysqlConnect();
 	
-	public FOrderDto getData(String orderid)
-	{
-		FOrderDto dto = new FOrderDto();
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		String sql="select * from forder where forderid=?";
-		
-		conn=db.getMyConnection();
-		try {
-			pstmt=conn.prepareStatement(sql);
-			pstmt.setString(1, orderid);
-			rs=pstmt.executeQuery();
-			if(rs.next())
-			{
-				dto.setFordernum(rs.getInt("fordernum"));
-				dto.setFmnname(rs.getString("fmnname"));
-				dto.setFsize(rs.getString("fsize"));
-				dto.setFtemp(rs.getString("ftemp"));
-				dto.setForderprice(rs.getInt("forderprice"));
-				dto.setForderdate(rs.getTimestamp("setforderdate"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}finally {
-			db.dbClose(conn, pstmt, rs);
-		}
-		return dto;
-	}
-	
-	public int getTotalCount() 
+	public int getTotalCount(String orderid) 
 	{
 		int n = 0;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql="select count(*) from forder";
+		String sql="select count(*) from forder where forderid=?";
 		conn=db.getMyConnection();
 		try {
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, orderid);
 			rs=pstmt.executeQuery();
 			if(rs.next())
 			{
@@ -66,15 +35,12 @@ public class FOrderDao {
 		}finally {
 			db.dbClose(conn, pstmt, rs);
 		}
-		
-		
 		return n;
 	}
 	
-	public List<FOrderDto> getOrderList(int start, int end)
+	public List<FOrderDto> getOrderList(int start, int end, String orderid)
 	{
-		String sql="select a.* from (select ROWNUM as RNUM,b.* from "
-				+ "(select * from forder order by fordernum desc)b)a where a.RNUM>=? and a.RNUM<=?";
+		String sql="select * from forder where forderid=? order by fordernum desc limit ?,?";
 		List<FOrderDto> list = new ArrayList<FOrderDto>();
 		
 		Connection conn = null;
@@ -84,10 +50,9 @@ public class FOrderDao {
 		conn=db.getMyConnection();
 		try {
 			pstmt=conn.prepareStatement(sql);
-			pstmt.setInt(1, start);
-			pstmt.setInt(2, end);
-			rs=pstmt.executeQuery();
-			
+			pstmt.setString(1, orderid);
+			pstmt.setInt(2, start);
+			pstmt.setInt(3, end);
 			rs=pstmt.executeQuery();
 			while(rs.next())
 			{
@@ -97,7 +62,7 @@ public class FOrderDao {
 				dto.setFsize(rs.getString("fsize"));
 				dto.setFtemp(rs.getString("ftemp"));
 				dto.setForderprice(rs.getInt("forderprice"));
-				dto.setForderdate(rs.getTimestamp("setforderdate"));
+				dto.setForderdate(rs.getTimestamp("forderdate"));
 				
 				//list에 추가
 				list.add(dto);
